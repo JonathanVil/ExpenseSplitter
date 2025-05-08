011ad0206c2e8dccce1b8fa0790804ca913fde9b
Jonathan Antoine Villeret <jvilleret@proton.me>
2025-05-08 16:28:24 +0200

    feat: add FastEndpoints security

--- ExpenseSplitter.API/Program.cs
+++ ExpenseSplitter.API/Program.cs
@@ -1,41 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using FastEndpoints;
+using FastEndpoints.Security;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var bld = WebApplication.CreateBuilder();
+bld.Services
+    .AddAuthenticationJwtBearer(s => s.SigningKey = "i'm a secret dont look at me")
+    .AddAuthorization()
+    .AddFastEndpoints();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+var app = bld.Build();
+app.UseAuthentication()
+    .UseAuthorization()
+    .UseFastEndpoints();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
