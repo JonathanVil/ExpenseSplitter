07a356ee4dfb726d1b008b879d4c58ee2a07c5fd
Jonathan Antoine Villeret <jvilleret@proton.me>
2025-05-09 21:04:59 +0200

    feat: persist db between runs

--- ExpenseSplitter.AppHost/Program.cs
+++ ExpenseSplitter.AppHost/Program.cs
@@ -3,10 +3,12 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
-    .WithPgWeb();
+    .WithPgWeb(pgWeb => pgWeb.WithHostPort(5050))
+    .WithDataVolume(isReadOnly: false);
 var postgresdb = postgres.AddDatabase("postgresdb");
 
 var api = builder.AddProject<ExpenseSplitter_API>("api")
-    .WithReference(postgresdb);
+    .WithReference(postgresdb)
+    .WaitFor(postgresdb);
 
 builder.Build().Run();
